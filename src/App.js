2483cd7f5ce1172577c81688bd76a63c69b05f95
Aycacalgin <calginayca@gmail.com>
2021-12-06 16:55:30 +0300

    kjhxdjkfh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './components/FormElementi.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+	<h1>Form Elementi</h1>
+  <div class="field">
+		<label for="Email">Email:</label>
+		<input type="text" class="text-center" placeholder="Lütfen e-posta adresinizi giriniz..." />
+
+    </div>
+    <div class="field"/>
+		<label for="Şifre">Şifre:</label>
+		<input type="text"  class="text-center" placeholder="Lütfen şifrenizi giriniz..." />
+     </form>
+     <input type="checkbox" class="text-center" name="" value="Şifremi hatırla" />
+     <label for="Şifremi hatırla">Şifremi hatırla</label>
+     
+     <button type="Gönder">Gönder</button>
     </div>
   );
 }
